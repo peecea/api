118a9df3ab7a82ef050cc48d7e283c9aabbc7a7f
ValicioKely <razalicio008@gmail.com>
2024-08-14 18:06:43 +0300

    Fix database indentifiant

--- internal/configuration/dev.go
+++ internal/configuration/dev.go
@@ -17,7 +17,7 @@ func InitiateConfiguration() (err error) {
 			Host:                    "",
 			RunningMode:             RunningModeProd,
 			TokenSecret:             "",
-			DatabaseUserName:        "root",
+			DatabaseUserName:        "admin",
 			DatabaseUserPassword:    "Wallicio2020#",
 			DatabaseName:            "peec",
 			DatabaseHost:            "peecdb.czko4g22s9mu.us-east-1.rds.amazonaws.com",
