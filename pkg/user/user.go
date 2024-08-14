a8f40bf70e1d1967632e8b409da5bab4f27986e5
ValicioKely <razalicio008@gmail.com>
2024-08-14 10:23:26 +0300

    Fix user model

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -39,27 +39,20 @@ const (
 )
 
 type User struct {
-	Id                    uint       `json:"id"`
-	CreatedAt             time.Time  `json:"created_at"`
-	UpdatedAt             time.Time  `json:"updated_at"`
-	DeletedAt             *time.Time `json:"deleted_at"`
-	Name                  string     `json:"name"`
-	FamilyName            string     `json:"family_name"`
-	NickName              string     `json:"nick_name"`
-	Email                 string     `json:"email"`
-	Matricule             string     `json:"matricule"`
-	Age                   uint       `json:"age"`
-	BirthDate             time.Time  `json:"birth_date"`
-	Sex                   int        `json:"sex"`
-	Lang                  int        `json:"language"`
-	Status                int        `json:"status"`
-	ProfileImageXid       string     `json:"profile_image_xid"`
-	Description           string     `json:"description"`
-	CoverText             string     `json:"cover_text"`
-	Profile               string     `json:"profile"`
-	ExperienceDetail      string     `json:"experience_detail"`
-	AdditionalDescription string     `json:"additional_description"`
-	AddOnTitle            string     `json:"add_on_title"`
+	Id              uint       `json:"id"`
+	CreatedAt       time.Time  `json:"created_at"`
+	UpdatedAt       time.Time  `json:"updated_at"`
+	DeletedAt       *time.Time `json:"deleted_at"`
+	Name            string     `json:"name"`
+	FamilyName      string     `json:"family_name"`
+	NickName        string     `json:"nick_name"`
+	Email           string     `json:"email"`
+	Matricule       string     `json:"matricule"`
+	Age             uint       `json:"age"`
+	BirthDate       time.Time  `json:"birth_date"`
+	Sex             int        `json:"sex"`
+	Status          int        `json:"status"`
+	ProfileImageXid string     `json:"profile_image_xid"`
 }
 
 /*
